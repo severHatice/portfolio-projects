726d16718300471f2dcf39a217f6bfc240fb12d0
severHatice <95497761+severHatice@users.noreply.github.com>
2023-10-11 13:00:48 +0200

    links added

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,22 @@
 const templateCard = document.querySelector('.card');
 
-cardDatas.forEach((card)=>{
-
+cardDatas.forEach((card) => {
    const clonedCard = templateCard.cloneNode(true);
 
-   clonedCard.querySelector('h2').innerText=card.title;
-   clonedCard.querySelector('p').innerText=card.description;
-   clonedCard.querySelector('img').src=card.imageUrl;
-//    clonedCard.style.backgroundImage=card.imageUrl
-   clonedCard.addEventListener('click',()=>{
-    window.location.href = card.url; 
+   clonedCard.querySelector('h2').innerText = card.title;
+   clonedCard.querySelector('p').innerText = card.description;
+   
+   const cardHeader = clonedCard.querySelector('.card__header');
+   cardHeader.style.backgroundImage = `url(${card.imageUrl})`;
+   
+   clonedCard.addEventListener('click', () => {
+      window.location.href = card.url; 
+   });
+   clonedCard.querySelector('button').addEventListener('click', () => {
+      event.stopPropagation();  
+      window.location.href = card.codeSource; 
    })
 
    document.querySelector('.card-container').append(clonedCard);
-
-})
+});
 templateCard.remove();
--- specs/cardData.js
+++ specs/cardData.js
@@ -3,31 +3,43 @@ const cardDatas =[
         title: "BrainFood",
         description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nisi ducimus sequi cumque sunt commodi.",
         imageUrl: "img/brain-food.png",
-        url:"brain-food/index.html"
+        url:"brain-food/index.html",
+        codeSource: "https://github.com/severHatice/portfolio-projects/tree/main/brain-food",
     },
     {
         title: "Awesome Card",
-        description: "...",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nisiyanan ducimus sequi cumque sunt commodi.",
         imageUrl: "img/brain-food.png",
-        url:"cards/awesome-card/index.html"
+        url:"cards/awesome-card/index.html",
+        codeSource:""
+    },
+    {
+        title: "Awesome Card",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nisi ducimus sequi cumque sunt commodi.",
+        imageUrl: "img/brain-food.png",
+        url:"cards/awesome-card/index.html",
+        codeSource:""
     },
     {
         title: "Forest Adventures",
-        description: "...",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nisi ducimus sequi cumque sunt commodi.",
         imageUrl: "img/forests-adventures.png",
-        url:"forests-adventures/index.html"
+        url:"forests-adventures/index.html",
+        codeSource:"https://github.com/severHatice/portfolio-projects/tree/main/forests-adventures"
     },
     {
         title: "Holiday Dream",
-        description: "...",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nisi ducimus sequi cumque sunt commodi.",
         imageUrl: "img/holiday-dream.png",
-        url:"share your holiday dream/index.html"
+        url:"share your holiday dream/index.html",
+        codeSource:"https://github.com/severHatice/portfolio-projects/tree/main/share%20your%20holiday%20dream"
     },
     {
         title: "Holiday Dream",
-        description: "...",
+        description: ".Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate, nisi ducimus sequi cumque sunt commodi.",
         imageUrl: "img/holiday-dream.png",
-        url:"share your holiday dream/index.html"
+        url:"share your holiday dream/index.html",
+        codeSource:"https://github.com/severHatice/portfolio-projects/tree/main/share%20your%20holiday%20dream"
     }
   
 ]
